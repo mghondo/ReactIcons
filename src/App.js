81b41ce3c213177a978b4c7d0e94448c6dc21ec1
Morgan Hondros <morganhondros@Morgans-MBP.localdomain>
2023-07-28 16:19:15 -0400

    added alarm icon and IconContext Provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { FaReact } from "react-icons/fa";
+import { MdAlarmAdd } from "react-icons/md";
+import { IconContext } from "react-icons/lib";
 
 function App() {
   return (
-    <div className="App">
-      <FaReact color="purple" size="10rem" />
-    </div>
+    <IconContext.Provider value={{ color: "blue", size: "5rem" }}>
+      <div className="App">
+        <FaReact />
+        <MdAlarmAdd />
+      </div>
+    </IconContext.Provider>
   );
 }
 
